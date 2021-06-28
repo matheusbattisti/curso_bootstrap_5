7099ea477f77a33052b325ee8d9a4b88dcc764e2
Matheus Battisti <matheusbudkewicz@gmail.com>
2021-06-28 00:02:43 -0300

    continuando curso

---
+++ 9_componentes/2_aulas/js/scripts.js
@@ -0,0 +1,11 @@
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
